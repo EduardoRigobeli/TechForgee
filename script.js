a1a25419aa2b4e847b2d3631b3f873e42b9313e4
EduardoRigobeli <eduardo.souza@grupointegrado.br>
2024-06-11 21:08:53 -0300

    atualizacao inteira do site (11/06)

--- script.js
+++
@@ -1,6 +0,0 @@
-function mostrarMenu() {
-    //guardar o elemento HTML para adicionar a class
-    var menu = document.querySelector(".header-nav");
-    //pega uma class - toggle troca .show
-    menu.classList.toggle("show");
-}
